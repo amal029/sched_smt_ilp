49b08a749c17a269ce45ef1c5e7fca97aaadc66a
Avinash Malik <avinash.malik@auckland.ac.nz>
2013-12-09 20:43:46 +1300

    Updated working code

--- gxl2smt.ml
+++ gxl2smt.ml
@@ -32,20 +32,12 @@ try
   let () = G.parse !file_name pp in
   let gxl_element = G.get_document_element pp in
   let graphs = GXL.get_gxl_gxl_graph_list gxl_element in
-  let () = IFDEF DEBUG THEN print_endline "Info about the graph!!\nGraph names:" ELSE () ENDIF in
-  let () = IFDEF DEBUG THEN L.iter (fun x -> print_endline (GXL.get_typed_element_id x)) graphs ELSE () ENDIF in
   let graph_attrs = L.map GXL.get_typed_element_attr_list graphs in
-  let () = IFDEF DEBUG THEN print_endline "Graph attributes :" ELSE () ENDIF in
-  let () = IFDEF DEBUG THEN 
-    L.iter (fun x -> L.iter (fun y -> print_endline ((GXL.get_attr_name y) ^ " = " ^ (GXL.get_attr_value y |> GXL.string_of_gxl_value))) x) graph_attrs 
-    ELSE () ENDIF in
   let graph_elements = L.map GXL.get_graph_element_list graphs in
 
 
   (* These are the graph nodes *)
   let graph_nodes_el = L.map (fun x -> L.filter (function | GXL.GXLNode _ -> true | _ -> false) x) graph_elements in
-  let () = IFDEF DEBUG THEN print_endline "Graph node names: " ELSE () ENDIF in
-  let () = IFDEF DEBUG THEN L.iter (fun x -> L.iter (fun y -> print_endline (GXL.get_graph_element_id y)) x) graph_nodes_el ELSE () ENDIF in
   let graph_nodes = L.map (fun x -> L.map (fun y -> "Node"^(GXL.get_graph_element_id y)) x) graph_nodes_el |> L.flatten in
   let declared_node_doc = (L.fold_left append empty) (L.map (fun x -> "(declare-fun " ^ x ^ " () Real)\n") graph_nodes |> L.map text)  in
   let () = IFDEF TDEBUG THEN print declared_node_doc ELSE () ENDIF in
@@ -65,7 +57,6 @@ try
   let node_weights = L.map (fun y -> L.map (fun x -> GXL.get_attr_value x |> GXL.string_of_gxl_value) y) node_weights in
   let ograph_nodes = L.map (fun x -> L.map (fun y -> (GXL.get_graph_element_id y)) x) graph_nodes_el |> L.flatten in
   let () = L.iter2 (fun x y -> H.add weighttbl x y) ograph_nodes node_weights in
-  let () = IFDEF DEBUG THEN H.iter (fun x y -> (x ^ "--" ^ (L.fold_left (fun t u -> t ^ " " ^ u) "" y) ) |> print_endline) weighttbl ELSE () ENDIF in
 
   (* These are the graph edges *)
   let graph_edges = L.map (fun x -> L.filter (function | GXL.GXLLocalConnection _ -> true | _ -> false) x) graph_elements in
@@ -85,10 +76,6 @@ try
 				       (* This needs to be changed for communication *)
 				       "(assert (>= Node" ^ t ^ " (+(+ Node" ^ s ^ " " ^ v ^ ")"^orew^")))\n" |> text)) in
   let () = IFDEF TDEBUG THEN print ea_doc ELSE () ENDIF in
-  let () = IFDEF DEBUG THEN print_endline "Connections: " ELSE () ENDIF in
-  let () = 
-    IFDEF DEBUG THEN L.iter (fun y -> L.iter (fun z -> (GXL.get_edge_source z |> GXL.get_graph_element_id) ^ " ---> " 
-					  ^ (GXL.get_edge_target z |> GXL.get_graph_element_id) |> print_endline) y) graph_edges ELSE () ENDIF  in
 	
   (* The final output to the SMT-LIB FORMAT *)
   let top = "(set-option :produce-proofs true)\n(set-logic QF_LRA)\n" |> text in

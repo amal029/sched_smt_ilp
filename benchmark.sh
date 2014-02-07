fcb9dfa39432ec80f9222e966ca8fa549a272872
Avinash Malik <avinash.malik@auckland.ac.nz>
2014-02-07 13:43:29 +1300

    Added the benchmarking bash script

---
+++ benchmark.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function benchmark () {
+    # Running the independent
+    for k in $2 ; do
+	for i in $1 ; do
+	    processors="2 4 8 16"
+	    for j in ${processors} ; do
+		file_name=$(echo $i | awk -F '/' '{print $2}' | awk -F '.' '{print $1}')
+		search=$(echo $k | awk -F '/' '{print $2}' | awk -F '.' '{print $1}')
+		echo $k -timeout 10000 -processors $j $i
+		sem -j4 $k -timeout 10000 -processors $j $i > $file_name.$search.$j.txt
+	    done
+	done
+    done
+}
+
+IND=`find ../ -iname 'Independent*.gxl'`
+RAND=`find ../ -iname 'Random*.gxl'`
+FORK=`find ../ -iname 'Fork_Node*.gxl'`
+JOIN=`find ../ -iname 'JOIN*.gxl'`
+STENCIL=`find ../ -iname 'STENCIL*.gxl'`
+
+# Do independent benchmark 
+benchmark "$IND" "./binary_search ./reduce_search"
+
+# # Do random benchmark 
+# benchmark "$RAND" "./binary_search ./reduce_search"
+
+# # Do fork benchmark 
+# benchmark  "$FORK" "./binary_search ./reduce_search"
+
+# # Do join benchmark 
+# benchmark "$JOIN" "./binary_search ./reduce_search"
+
+# # Do STENCIL benchmark 
+# benchmark "$STENCIL" "./binary_search ./reduce_search"
+

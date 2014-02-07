515f934bb866418765b8562a0e08fa0e9078d472
Avinash Malik <avinash.malik@auckland.ac.nz>
2014-02-07 16:34:58 +1300

    Updated the benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -3,6 +3,7 @@
 rm -rf *.txt results
 mkdir -p results
 
+count=1
 function benchmark () {
     # Running the independent
     for k in $2 ; do
@@ -11,32 +12,46 @@ function benchmark () {
 	    for j in ${processors} ; do
 		file_name=$(echo $i | awk -F '/' '{print $2}' | awk -F '.' '{print $1}')
 		search=$(echo $k | awk -F '/' '{print $2}' | awk -F '.' '{print $1}')
-		echo $k -timeout 30000 -processors $j $i
-		sem -j4 $k -timeout 10000 -processors $j $i > ./results/$file_name.$search.$j.txt
+		echo $k -timeout 10000 -processors $j $i
+		count=$(($count + 1))
+		sem -j4 $k -timeout 10000 -processors $j $i > ./results/$file_name.$search.$j.$count.txt
 	    done
 	done
     done
 }
 
+SERIES=`find ../ -iname 'Series*.gxl'`
+PIPE=`find ../ -iname 'Pipeline*.gxl'`
 IND=`find ../ -iname 'Independent*.gxl'`
 RAND=`find ../ -iname 'Random*.gxl'`
 FORK=`find ../ -iname 'Fork_Node*.gxl'`
 JOIN=`find ../ -iname 'JOIN*.gxl'`
 STENCIL=`find ../ -iname 'STENCIL*.gxl'`
 
+# # # Do random benchmark 
+benchmark "$SERIES" "./binary_search"
+
+# # # Do random benchmark 
+benchmark "$PIPE" "./binary_search"
+
 # Do independent benchmark 
-benchmark "$IND" "./binary_search ./reduce_search"
+# benchmark "$IND" "./binary_search ./reduce_search"
+benchmark "$IND" "./binary_search"
 
 # Do fork benchmark 
-benchmark  "$FORK" "./binary_search ./reduce_search"
+benchmark  "$FORK" "./binary_search"
+# benchmark  "$FORK" "./binary_search ./reduce_search"
 
 # Do join benchmark 
-benchmark "$JOIN" "./binary_search ./reduce_search"
+benchmark "$JOIN" "./binary_search"
+# benchmark "$JOIN" "./binary_search ./reduce_search"
 
 # Do STENCIL benchmark 
-benchmark "$STENCIL" "./binary_search ./reduce_search"
+benchmark "$STENCIL" "./binary_search" 
+# benchmark "$STENCIL" "./binary_search ./reduce_search"
 
 
 # # # Do random benchmark 
+benchmark "$RAND" "./binary_search"
 # benchmark "$RAND" "./binary_search ./reduce_search"
 
